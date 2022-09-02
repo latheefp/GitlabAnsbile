dbe3b6792304681b9554857b9cc5d0574b515abe
Abdul Latheef <latheefp@latheef-mac.local>
2022-09-02 22:28:34 +0300

    working dns configuration

--- dockerdns/build.sh
+++ dockerdns/build.sh
@@ -1,13 +1,9 @@
+#!/bin/bash
 VER=2.9
-#docker build -t latheef/bind9:$VER -f Dockerfile.$VER
 rm etc/bind/*
 docker stop dns-server
-docker rm  image latheefp/bind9:2.9
+docker rm  image latheefp/bind9:$VER
 docker build -t latheefp/bind9:$VER . 
-#docker stop dns-server; docker rm dns-server
-#docker run  -d --rm --name=dns-server --net=dns-net --ip=172.20.0.2 -p 53:53/udp etc/bind:/etc/named/conf -p 53:53/tcp latheefp/bind9:$VER
-docker run  -d --rm --name=dns-server --net=dns-net --ip=172.20.0.2 -p 53:53/udp --volume=`pwd`/etc/bind:/etc/bind/conf -p 53:53/tcp latheefp/bind9:2.9 
-#docker run  -d --rm -ti --name=dns-server --net=dns-net --ip=172.20.0.2 -p 53:53/udp --volume=`pwd`/etc/bind:/etc/bind/conf -p 53:53/tcp latheefp/bind9:2.9
-#docker run  -d  -ti --name=dns-server --restart on-failure --net=dns-net --ip=172.20.0.2 -p 53:53/udp --volume=`pwd`/etc/bind:/etc/bind/conf -p 53:53/tcp latheefp/bind9:2.9
+docker run  -d --rm --name=dns-server --net=dns-net --ip=172.20.0.2 -p 53:53/udp --volume=`pwd`/etc/bind:/etc/bind/conf -p 53:53/tcp -e DOMAIN=egrand.in latheefp/bind9:2.9 
+docker push latheefp/bind9:$VER
 docker exec -it dns-server sh
-#docker push latheefp/bind9:$VER
--- dockerdns/entrypoint.sh
+++ dockerdns/entrypoint.sh
@@ -1,34 +1,38 @@
 #!/bin/bash
 CONFPATH=/etc/bind/conf
 SRCPATH=/etc/bind/default
-#echo $DOMAIN
-#RZONE=$(host $(ifconfig eth0|grep inet|awk {'print $2'})|awk {'print $1'}|cut -d "." -f2-6) 
-#echo $RZONE
-DOMAIN=riyadh.lan
-RZONE=rev.0.20.172.in-addr.arp
-echo "============== Afer copy ============="
-ls -ltr /etc/bind/conf/
+echo Domain is  $DOMAIN
+RZONE=$(host $(ifconfig eth0|grep inet|awk {'print $2'})|awk {'print $1'}|cut -d "." -f2-6) 
+echo nameserver 127.0.0.1 >/etc/resolv.conf
+#echo options ndots:0 >> /etc/resolv.conf
+echo "search $DOMAIN" >>/etc/resolv.conf
+
 
 if [ ! -d $CONFPATH ]; then
  echo "$CONFPATH  not exists, exiting"
  exit 1
 fi
 
-if [ ! -f $CONFPATH/db.$DOMAIN ]
-then
-   cp $SRCPATH/db.def $CONFPATH/db.$DOMAIN
+echo checking $CONFPATH/$DOMAIN
+if [ ! -f $CONFPATH/$DOMAIN ]
+   then
+    cp $SRCPATH/db.def $CONFPATH/$DOMAIN
 fi
 
+echo checking $CONFPATH/named.conf.local 
 if [ ! -f $CONFPATH/named.conf.local ]
 then
    cp $SRCPATH/named.conf.local $CONFPATH/
 fi
 
-if [ ! -f $CONFPATH/db.$RZONE ]
+echo checking $CONFPATH/$RZONE
+
+if [ ! -f $CONFPATH/$RZONE ]
 then
-   cp $SRCPATH/rev.def $CONFPATH/db.$RZONE
+   cp $SRCPATH/rev.def $CONFPATH/$RZONE
 fi
 
+echo checking $CONFPATH/named.conf.options
 
 if [ ! -f $CONFPATH/named.conf.options ]
 then
@@ -37,12 +41,12 @@ fi
 
 sed -i -e  "s/local.lan/$DOMAIN/g" $CONFPATH/named.conf.local 
 sed -i -e "s/rev.local/$RZONE/g" $CONFPATH/named.conf.local 
+sed -i -e "s/local.lan/$DOMAIN/g" $CONFPATH/$DOMAIN
+sed -i -e "s/local.lan/$DOMAIN/g" $CONFPATH/$RZONE
+
 
 echo include \"$CONFPATH/named.conf.options\"\; >/etc/bind/named.conf
 echo include \"$CONFPATH/named.conf.local\"\; >>/etc/bind/named.conf
-echo include \"$CONFPATH/named.conf.default-zones\"\; >>/etc/bind/named.conf
-
-echo "============== Afer copy ============="
-ls -ltr /etc/bind/conf/
+echo include \"/etc/bind/named.conf.default-zones\"\; >>/etc/bind/named.conf
 
 service bind9 start ; while true; do sleep 100; done;
